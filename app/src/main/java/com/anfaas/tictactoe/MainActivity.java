939562e96a29cadaf03fe20a8338e3072c0d99eb
anfaas1618 <ishad0911@gmail.com>
2019-12-03 23:29:32 +0530

    Initial commit nothing in it

---
+++ app/src/main/java/com/anfaas/tictactoe/MainActivity.java
@@ -0,0 +1,15 @@
+package com.anfaas.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
